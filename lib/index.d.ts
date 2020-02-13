7e3d491e6cb2c54cc1170b133005d2c93c9e4d99
pierre <dommerc.pierre@gmail.com>
2020-02-13 16:31:43 +0100

    fix missing files

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+import { Color } from 'ora';
+import { SpinnerName } from 'cli-spinners';
+interface HookConfig {
+    name: string;
+    buildStart(): void;
+    transform(code: string, id: string): null;
+    renderStart(): void;
+    writeBundle(): void;
+}
+interface Options {
+    spinner?: SpinnerName;
+    color?: Color;
+    indent?: number;
+}
+export default function loading(options?: Options): HookConfig;
+export {};
